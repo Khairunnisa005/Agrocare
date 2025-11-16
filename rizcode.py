8c02bec2588b1bbffb8ebaab2daa31145e81a341
Miftakhur Rizqi <mrrizqiiiii@gmail.com>
2025-11-16 11:39:40 +0700

    menu regist

--- rizcode.py
+++ rizcode.py
@@ -1,3 +1,24 @@
-print("Rizqi")
-print("aca")
-print("nesya")
+
+#import os
+import pandas as pd
+
+#def register():
+#os.system('cls')
+
+username = input("Buat Username: ")
+password = input("Buat Pasword: ")
+
+
+data_akun = pd.read_csv('users.csv')
+
+role = "pembeli"
+while True :
+    if (data_akun['username'] == username,data_akun['password'] == password).any():
+        print("akun sudah pernah di buat !!!")
+        #register()
+    else :
+        menambahkan_datauser = pd.DataFrame({'username': [username], 'password': [password], 'role': [role]})
+        data_user = data_user._append(menambahkan_datauser)
+        data_user.to_csv('users.csv', index=False)
+        print("akun berhasil di buat")
+        #menu_login
