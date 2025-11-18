4d388906cbed48d671a56d201a1658fb902db0f6
Aca <khairunnisabhari@gmail.com>
2025-11-18 20:31:19 +0700

    merge

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 def register():
-
+    os.system('cls')
     username = input("Buat Username: ")
     #while ini untuk memeastikan supaya input tidak kososng 
     #program tidak lanjut ke password jika tidak mengisi user name 
@@ -24,6 +24,7 @@ def register():
     #aku hapus yang paswwor nyaa aja kalok paswword sama ndak papa yang penting username nya aja beda
     if akun_sama:
         print("username sudah pernah terdaftar")
+        register()
     else:
         # Buat row baru
         row_baru = pd.DataFrame({
@@ -39,10 +40,9 @@ def register():
         data_akun.to_csv('users.csv', index=False)
         print("Akun berhasil dibuat")
 
-register()
 
 def login():
-
+    os.system('cls')
 # Baca CSV
     data_akun = pd.read_csv("users.csv")
 
@@ -57,29 +57,51 @@ def login():
 
     if akun.empty:
         print("\n Username tidak terdaftar. Silahkan daftarr/register telebih dahulu.") 
-        exit() #untuk stop program jika usn belum terdaftar
+        return() 
 
     #kalo usn benar minta passwod
     password = input("Masukkan Password : ")
 
-#cek pw
+    #cek apakah pw dan usn benar
     password_benar = data_akun[
         (data_akun["username"] == username) & 
         (data_akun["password"] == password)
     ]
-
+    #cek pw
     if password_benar.empty: 
         print("\n Password salah! Silahkan coba lagi.")
-        exit()#untuk stop program jika paw salah
+        return() #jika pw salah mka kembali ke menu
 
-    # jika usn dan pw benar maka login berhasil
+    # jika usn dan pw benar cek role maka login berhasil
     role = password_benar.iloc[0]["role"]
 
-            # Tampilan selamat datang
-    if role == "admin dan pembeli":
-        print("\n Login berhasil! Selamat datang ADMIN,", username)
+    # Tampilan selamat datang sesuai role
+    if role == "admin":
+        print(f"\n Login berhasil! Selamat datang ADMIN,", username)
     else:
         print("\n Login berhasil! Selamat datang di Agrocare,", username)
-        
-login()
-        
+
+
+def menu():
+        while True: # menu akan terus mucul sampai user memilih keluar
+            print("========================================================")
+            print("===============selamat datang di Agrocare===============")
+            print("========================================================")
+            print("1. Login")
+            print("2. Register")
+            print("3. Keluar")
+            print("========================================================")
+
+            pilihan = input("pilih menu (1/2/3):")
+
+            if pilihan == "1":
+                login()
+            elif pilihan == "2":
+                register()
+            elif pilihan == "3":
+                print("Terimakasih telah menggunakan Agrocare. Sampai jumpa!")
+                break #keluar dari while true / menghentikan program
+            else:
+                print("\n Pilihan tidak valid! Silahkan masukkan angka 1-3.\n")
+
+menu()  
